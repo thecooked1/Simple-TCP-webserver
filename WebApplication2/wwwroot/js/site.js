8724639605b231d68656bb5ca8ea695562255e18
Ioane Chanturia <ioane.chanturia@gmail.com>
2025-05-31 20:35:05 +0400

    Initial setup, Project uploaded, Added index.html and about.html

---
+++ WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿console.log("Script loaded!");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heading = document.querySelector('h1');
+    if (heading) {
+        heading.addEventListener('click', () => {
+            alert('You clicked the heading: ' + heading.textContent);
+        });
+    }
+});
